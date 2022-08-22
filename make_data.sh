f1fba6dba339704bb1388e9e9a9b5852a2eeeb51
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-08-22 15:11:38 +0200

    Adding shacl examples

--- make_data.sh
+++ make_data.sh
@@ -4,3 +4,8 @@ for file in data/solidui/*.ttl; do
     echo "$file..."
     eye --quiet --nope --pass-only-new $file rules/solidui2rdfform.n3 > $file.out 2> /dev/null
 done
+
+for file in data/shacl/*.ttl; do
+    echo "$file..."
+    eye --quiet --nope --pass-only-new $file rules/shacl2rdfform.n3 > $file.out 2> /dev/null
+done
