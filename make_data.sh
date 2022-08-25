b0d5a2a40ef92e1bebdff93c6c1dd09e5f80d68a
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2022-08-25 21:13:31 +0200

    Adding a sparql version of the solidui rules

--- make_data.sh
+++ make_data.sh
@@ -2,7 +2,8 @@
 
 for file in data/solidui/*.ttl; do
     echo "$file..."
-    eye --quiet --nope --pass-only-new $file rules/solidui2rdfform.n3 > $file.out 2> /dev/null
+    eye --quiet --nope --pass-only-new $file rules/solidui2rdfform.n3 > $file.eye.out 2> /dev/null
+    comunica-sparql-file -f rules/solidui2rdfform.sparql $file > $file.comunica.out 2> /dev/null
 done
 
 for file in data/shacl/*.ttl; do
